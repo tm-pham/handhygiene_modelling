b6d93469ecaa542c1b30679201fd6886ed9f301a
tm-pham <45562242+tm-pham@users.noreply.github.com>
2020-03-17 12:39:59 +0100

    Add files via upload

--- covid-19_handhygiene.R
+++ covid-19_handhygiene.R
@@ -1,6 +1,7 @@
 # =============================================================================== #
 # Hand hygiene covid-19 model 
 # =============================================================================== #
+rm(list=ls())
 # Packages
 library(ggplot2)
 library(tidyr)
@@ -56,7 +57,7 @@ lognormal.times <- function(t_end, c_mean, c_sd){
 # CT = Option for distribution of carriage duration
 # =============================================================================== #
 
-household.fun <- function(hh_num=1,t_end=24, 
+household.fun <- function(t_end=24*5, 
                           CT = 2, c_mean=1, c_sd=1, halflife=1, 
                           f_rate=2, eps=0.01,
                           HW = 1, hw_mean=10/60,
@@ -65,56 +66,65 @@ household.fun <- function(hh_num=1,t_end=24,
   # rate for exponential decay computed from halflife
   v_rate <- -log(0.5)/halflife
   
-  p_inf <- rep(NA, hh_num)
-  for(h in 1:hh_num){
-    # Contamination times
-    if(CT==1){ # Fixed times (constant)
-      t_c<- seq(0, t_end, by = c_mean)
-    }else if(CT==2){ # Exponential distrbution
-      t_c <- exp.times(t_end,1/c_mean)$t
-    }else if(CT==3){ # Log-normal (heavy-tail)
-      t_c <- lognormal.times(t_end,c_mean,c_sd)$t
-    }
-
-    
-    # Hand washing times/events
-    t_HW <-NULL
-    if(HW==1){# Hand washing at fixed times
-      t_HW <- seq(0, t_end, by = hw_mean)
-    }else if(HW==2){# Hand washing at random times (Poisson)
-      t_HW <- exp.times(t_end, 1/hw_mean)$t
-    }else if(HW==3){# Event-prompted hand washing
-      # TBD
-    }
-    
-    # Face-touching events
-    # For now: poisson distribution for events/exponential distribution for times
-    t_f <- exp.times(t_end, f_rate)$t
-
-    # Compute probability of persistence at face-touching events
-    # For now: Exponential distribution 
-    v <- rep(0, length(t_f)) # prob. of contamination at face touching events
-    for(i in 1:length(t_f)){
-      ind <- which(t_c<=t_f[i])
-      if(length(ind)>0){
-        # Last contamination event/time
-        last <- t_c[ind[length(ind)]] 
-        if(HW==0) v[i] <- exp(-v_rate*(t_f[i]-last)) # No handwashing events
+  # Contamination times
+  if(CT==1){ # Fixed times (constant)
+    t_c<- seq(0, t_end, by = c_mean)
+  }else if(CT==2){ # Exponential distrbution
+    t_c <- exp.times(t_end,1/c_mean)$t
+  }else if(CT==3){ # Log-normal (heavy-tail)
+    t_c <- lognormal.times(t_end,c_mean,c_sd)$t
+  }
+  
+  
+  # Hand washing times/events
+  t_HW <-NULL
+  if(HW==1){# Hand washing at fixed times
+    t_HW <- seq(0, t_end, by = hw_mean)
+  }else if(HW==2){# Hand washing at random times (Poisson)
+    t_HW <- exp.times(t_end, 1/hw_mean)$t
+  }else if(HW==3){# Event-prompted hand washing
+    # TBD
+  }
+  
+  # Face-touching events
+  # For now: poisson distribution for events/exponential distribution for times
+  t_f <- exp.times(t_end, f_rate)$t
+  
+  # Compute probability of persistence at face-touching events
+  # For now: Exponential distribution 
+  v <- rep(0, length(t_f)) # prob. of contamination at face touching events
+  # total time of exposure, i.e. cumulative time between contamination event and face-touching 
+  # with no hand washing event: 
+  t_exp <- last_tc <- NULL
+  for(i in 1:length(t_f)){
+    ind <- which(t_c<=t_f[i])
+    if(length(ind)>0){
+      # Last contamination event/time
+      last <- t_c[ind[length(ind)]] 
+      if(HW==0){
+        v[i] <- exp(-v_rate*(t_f[i]-last)) # No handwashing events
+        t_exp <- c(t_exp, t_f[i]-last)
+        last_tc <- c(last_tc,last)
+      }
+      else{
+        # Was there a hand washing in between contamination and face-touching?
+        ind_HW <- intersect(which(t_HW>=last), which(t_HW<t_f[i]))
+        if(length(ind_HW)>0) v[i] <- 0 # yes
         else{
-          # Was there a hand washing in between contamination and face-touching?
-          ind_HW <- intersect(which(t_HW>=last), which(t_HW<t_f[i]))
-          if(length(ind_HW)>0) v[i] <- 0 # yes
-          else v[i] <- exp(-v_rate*(t_f[i]-last)) # no
+          v[i] <- exp(-v_rate*(t_f[i]-last)) # no
+          t_exp <- c(t_exp, t_f[i]-last)
+          last_tc <- c(last_tc,last)
         }
       }
     }
-    
-    # Force of infection
-    foi <- eps*v
-    # Probability of infection
-    p_inf[h] <- 1-exp(-sum(foi))
   }
-  return(list(p_inf=p_inf, v=v, t_c=t_c, t_f=t_f, t_HW=t_HW))
+  
+  # Force of infection
+  foi <- eps*v
+  # Probability of infection
+  p_inf <- 1-exp(-sum(foi))
+  
+  return(list(p_inf=p_inf, v=v, t_c=t_c, t_f=t_f, t_HW=t_HW, t_exp=t_exp, last_tc=last_tc))
 }
 
 # Calculate epsilon (prob. of transmission per contact) in scenario with no HW
@@ -137,13 +147,13 @@ compute.eps <- function(t_end,c_mean,f_rate,halflife, SAR=0.5, seed=NULL){
 
 
 ###############################
-
+infPeriod <- 5
 # Without hand washing
-dc <- seq(1,24)
+dc <- seq(1,30)/60 # Persistence of virus on hands (per hour)
 eps <- probTrans <- meanP <- rep(0,length(dc))
 for(i in 1:length(dc)){
-  eps[i] <- compute.eps(t_end=24,c_mean=1,f_rate=11,halflife=dc[i], SAR=0.5, seed=12345)
-  p <- household.fun(hh_num=1, HW=0, f_rate=11, eps=eps[i], halflife=dc[i],seed=12345)$v
+  eps[i] <- compute.eps(t_end=24*infPeriod,c_mean=1,f_rate=11,halflife=dc[i], SAR=0.5, seed=12345)
+  p <- household.fun(t_end=24*infPeriod,hh_num=1, HW=0, f_rate=11, eps=eps[i], halflife=dc[i],seed=12345)$v
   meanP[i] <- mean(p)
 }
 
@@ -159,13 +169,13 @@ plot(dc, eps,
 # =============================================================================== #
 # hw <- c(1,2,3,6,12,15,20)
 hw <- c(1,2,4,8,16)
-dc <- seq(1,24)
+dc <- seq(1,30)/60
 data <- dataInf <- NULL
 for(h in hw){
   eps <- probTrans <- meanP <- pInf <- rep(0,length(dc))
   for(i in 1:length(dc)){
-    eps[i] <- compute.eps(t_end=24,c_mean=1,f_rate=11,halflife=dc[i], seed=12345)
-    pInf[i] <- household.fun(hh_num=1, HW=1,hw_mean=h, f_rate=11, eps=eps[i], halflife=dc[i],seed=12345)$p_inf
+    eps[i] <- compute.eps(t_end=24*infPeriod,c_mean=1,f_rate=11,halflife=dc[i], seed=12345)
+    pInf[i] <- household.fun(t_end=24*infPeriod,hh_num=1, HW=1,hw_mean=h, f_rate=11, eps=eps[i], halflife=dc[i],seed=12345)$p_inf
   }
   dataInf <- cbind(dataInf, pInf)
 }
@@ -177,10 +187,12 @@ dataInf %>% gather() %>% group_by(key) %>%
   mutate(x=1:n()) %>%
   ggplot(aes(x=x, y=value,group=key,color=key)) + 
   geom_line() +
-  xlab("Half life of probability of persistence(hours)") + 
+  xlab("Half life of probability of persistence (minutes)") + 
   ylab("Cumulative probability of infection") + 
+  # scale_x_discrete(breaks=xaxis, labels=as.character(xaxis)) + 
   scale_color_discrete(name="Hand hygiene frequency \nEvery .. hours",
                        labels=hw)
+
 
 # =============================================================================== #
 # plot of how many transmission events (in a population of 100 two person 
@@ -188,37 +200,45 @@ dataInf %>% gather() %>% group_by(key) %>%
 # minutes in the absence of hand hygiene (so x axis is minutes, y axis is 
 # proportion of transmission events)
 # =============================================================================== #
-dc <- seq(1,2*60,by=2)/60
-p <- rep(0, length(dc))
-for(i in 1:length(dc)){
-  p[i] <- mean(household.fun(hh_num=100, HW=0, f_rate=11, eps=0.001, halflife=dc[i],seed=12345)$p_inf)
+epsilon <- 0.001 # Probability of transmission per contact
+num <- 100 # Number of households
+dc <- c(1,3,5,10,15)/60 # Half life of probability of persistence
+propTransMat <- NULL
+maxExp <- 6
+xaxis <- seq(3/60,maxExp,by=3/60) # Discretization of time
+for(d in dc){
+  pinf <- rep(0,num)
+  texp <- v <- last_tc <- list()
+  for(h in 1:num){
+    sim <- household.fun(t_end=24*infPeriod, HW=0, f_rate=11, eps=epsilon, halflife=d)
+    pinf[h] <- sim$p_inf
+    texp[[h]] <- sim$t_exp
+    v[[h]] <- sim$v
+    last_tc[[h]] <- sim$last_tc
+  }
+  v <- lapply(v, function(x) x[which(x!=0)])
+  
+  # maxExp <- max(c(maxExp, unlist(lapply(texp, function(x) max(round(x,2))))))
+  propTrans <- rep(0, length(xaxis))
+  for(i in 1:length(xaxis)){
+    temp <- NULL
+    for(h in 1:num){
+      temp<- c(temp, 1-exp(-sum(epsilon*v[[h]][which(texp[[h]]<xaxis[i])])))
+    }
+    propTrans[i] <- sum(temp)/num
+  }
+  propTransMat <- cbind(propTransMat, propTrans)
 }
-plot(dc, p, 
-     xlab="Half life of probability \n of persistence(hours)", ylab="Proportion of transmission events",
-     main="No hand washing")
 
-
-# =============================================================================== #
-# exploration of whether distribution of duration of carriage duration matters. 
-# i.e. are results the same if duration is constant or exponential 
-# (with the same mean) - or heavy-tailed 
-# =============================================================================== #
-dc <- seq(1,2*60,by=2)/60
-p <- q <- r <- rep(0, length(dc))
-for(i in 1:length(dc)){
-  p[i] <- mean(household.fun(hh_num=100, HW=0, CT=1, c_mean=1,f_rate=11, eps=0.001, halflife=dc[i],seed=12345)$p_inf)
-  q[i] <- mean(household.fun(hh_num=100, HW=0, CT=2, c_mean=1,f_rate=11, eps=0.001, halflife=dc[i],seed=12345)$p_inf)
-  r[i] <- mean(household.fun(hh_num=100, HW=0, CT=3, c_mean=1,c_sd=1, f_rate=11, eps=0.001, halflife=dc[i],seed=12345)$p_inf)
-}
-
-dataSens <- as.data.frame(cbind(p,q,r)) 
-colnames(dataSens) <- 1:ncol(dataSens)
-dataSens %>% gather() %>% group_by(key) %>% 
+propTransMat <- as.data.frame(propTransMat)
+colnames(propTransMat) <- 1:ncol(propTransMat)
+rownames(propTransMat) <- 1:nrow(propTransMat)
+propTransMat %>% gather() %>% group_by(key) %>% 
   mutate(x=1:n()) %>%
   ggplot(aes(x=x, y=value,group=key,color=key)) + 
   geom_line() +
-  xlab("Half life of probability \n of persistence(hours)") + 
+  xlab("Time of contaminated hands after contamination event (minutes)") + 
   ylab("Proportion of transmission events") + 
-  scale_color_discrete(name="Distribution",
-                       labels=c("Uniform","Exponential","Lognormal"))
-
+  # scale_x_discrete(breaks=xaxis, labels=as.character(xaxis)) + 
+  scale_color_discrete(name="Half life of probability \nof persistence (minutes)",
+                       labels=as.character(dc*60))
